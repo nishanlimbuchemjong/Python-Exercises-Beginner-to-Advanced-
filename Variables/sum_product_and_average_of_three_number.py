fcb78866b57604f292eb18d8c33ef78938503253
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-30 13:02:25 +0545

    sum_product_and_average_three_number.py file created; Total, Average, and Product of three number calculated

---
+++ Variables/sum_product_and_average_of_three_number.py
@@ -0,0 +1,12 @@
+"""
+Q. Calculate the total, average, and product of three numbers stored in variables x, y, and z. Print the results.
+"""
+x = 5
+y = 6
+z = 7
+
+total = x + y + z
+average = total / 3
+product = x * y * z
+
+print(f"Results:\nTotal = {total}\nAverage = {average}\nProduct = {product}")
