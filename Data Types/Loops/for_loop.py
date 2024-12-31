f2a1fa2d71a45443173f7022d31e14fb5b44519f
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-31 15:31:13 +0545

    questions related to for loop solved on for_loops.py file of Loops folder

---
+++ Data Types/Loops/for_loop.py
@@ -0,0 +1,47 @@
+"""
+All Questions Related to 'For Loop in Python'
+"""
+
+# Q.1) Write a program to calculate the sum of all even numbers from 1 to 100 using a for loop.
+print("Question-1: Sum of Even Numbers\n")
+
+# initial value of sum 
+sum = 0
+
+# using for loop for calculating the sum of all even numbers from 1 to 100
+for i in range(1, 101):
+    if i % 2 == 0:    #checking if the number is even or not
+        sum += i
+print(f"Sum of even numbers from 1 to 100 = {sum}")
+print("......................................................\n")
+
+# Q.2) Write a program that prints all multiples of any number taken from user using a for loop.
+print("Question-2: Multiples of Given Number")
+
+# taking a number from a user for a multiplication table
+user_number = int(input("Enter the number for a multiplication table: "))
+
+# calculating and printing the multiplication table of a given number
+print(f"\nMultiplication Table of {user_number}:")
+for i in range(1, 11):
+    print(f"{user_number} X {i} = {user_number * i}")
+
+print("................................................\n")
+
+# Q.3) Write a program that finds the maximum number entered by the users using a for loop.
+print("Question-3: Find Maximum from Users Input")
+
+# taking numbers from the user
+total_number = int(input("Enter total number to entered: "))
+
+# creating an empty list for storing the numbers that user have entered
+nums = []
+
+print("Enter your numbers: ")
+for i in range(total_number):
+    nums.append(int(input()))
+print(f"\nNumbers you have given: {nums}")
+
+nums.sort()
+print(f"\nResult:\tMaximum number you have entered: {nums[-1]}")
+print("...........................................................\n")
