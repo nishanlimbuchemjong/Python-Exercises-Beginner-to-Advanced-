6bf091414f64e6c8d02da0f89ff7d8c442a231bc
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-31 18:20:18 +0545

    Sorted values using sorted() built-in function on sorted_values.py file

---
+++ Built-In Functions/sorted_values.py
@@ -0,0 +1,10 @@
+# Use the sorted() function to sort the dictionary {1: 'b', 2: 'a', 3: 'c'} by its values.
+
+dictionary = {1: 'b', 2: 'a', 3: 'c'}
+
+# sorting the values of a given dictionary using sorted() built-in function
+result = sorted(dictionary.values())
+
+# printing out the result
+print(f"Given dictionary is {dictionary}")
+print(f"\nAfter using sorted() function for sorting above dictionary, we get\n{result}")
