98b05a424d3549894866e100468da7450c7eba51
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-01 14:18:38 +0545

    Merged two dictionaries on q.no.2 on all_question.py file of Dictionary folder

--- Data Types/Dictionary/all_question.py
+++ Data Types/Dictionary/all_question.py
@@ -4,3 +4,15 @@ dict_numbers = {x:x**2 for x in range(1, 6)}
 
 print(f"Dictionary created where keys are numbers from 1 to 5, and values are their squares:\n\t{dict_numbers}")
 print(".................................................................\n")
+
+# Q.2) Write a program to merge two dictionaries.
+print("Question-2:\n")
+
+number_dict = {"Name":"Nishan", "Grade": "BCA 7th Sem"}
+fav_sub_dict = {"Books": ["Science", "Mathematics", "Optional Math"]}
+
+number_dict.update(fav_sub_dict)
+
+print(f"Number Dictionary = {number_dict}\nFavourite Subject Dictionary = {fav_sub_dict}")
+print(f"\nAfter merging two dictionaries, we get \n\t{number_dict}")
+print("...............................................................\n")
