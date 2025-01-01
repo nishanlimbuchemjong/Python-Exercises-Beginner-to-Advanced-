fdc6cfc4023fcb5096889cab18d542344545b3e2
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-01 14:13:08 +0545

    Dictionary created wehre keys are numbers and values are their squares on q.1 of all_quesiton.py file inside Dictionary folder

---
+++ Data Types/Dictionary/all_question.py
@@ -0,0 +1,6 @@
+# Q.1) Create a dictionary where keys are numbers from 1 to 5, and values are their squares.
+print("Question-1:\n")
+dict_numbers = {x:x**2 for x in range(1, 6)}
+
+print(f"Dictionary created where keys are numbers from 1 to 5, and values are their squares:\n\t{dict_numbers}")
+print(".................................................................\n")
