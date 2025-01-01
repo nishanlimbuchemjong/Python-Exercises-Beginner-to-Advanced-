d80ac6e627501bc13d5031a7c51d8c37cc1737ae
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-01 15:11:51 +0545

    Normal Function added to find the factorial of a number in factorial_number.py inside Functions folder

--- Data Types/Functions/factorial_number.py
+++ Data Types/Functions/factorial_number.py
@@ -1,5 +1,12 @@
 # Q) Write a function factorial that takes a number and returns its factorial.
 
+# Method-1: Recursive Function
+def get_number_integer(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("\nInvalid! Enter integer number!!!\n")
 
 def factorial(num):
     """
@@ -12,10 +19,29 @@ def factorial(num):
         return num * factorial(num-1)
 
 # taking number from the user
-user_number = int(input("Enter number to find factorial: "))
+user_number = get_number_integer("Enter number to find factorial: ")
 
 # calling the factorial() function
 result = factorial(user_number)
 
 # printing the factorial of users entered number
 print(f"Factorial of {user_number} is {result}")
+
+print(".....................................................................\n")
+
+# Method-2: Normal Function
+def factorial(num):
+    fact = 1
+    if num == 0:
+        return 1
+    if num == 1:
+        return 1
+    else:
+        for i in range(1, num+1):
+            fact *= i
+        return fact
+
+num = get_number_integer("Enter a number to find the factorial: ")
+result = factorial(num)
+print(f"Factorial of {num}! = {result}")
+print("................................................................\n")
