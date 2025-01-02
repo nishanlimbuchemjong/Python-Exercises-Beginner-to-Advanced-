33753718dffff7472a523809e55a06f867462bef
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-02 13:58:28 +0545

    q.11, 12, 13, 14, 15 done on 20_questions.py file

--- Data Types/Functions/20_questions.py
+++ Data Types/Functions/20_questions.py
@@ -1,5 +1,5 @@
 # Q.1) Write a function that takes a string and returns the number of characters in it
-
+print("Question-1:\n")
 def number_of_characters(sentence):
     total_character = len(sentence)
     return total_character
@@ -11,7 +11,7 @@ print(f"Total characters in '{sentences}'\n{result}")
 print("..............................................\n")
 
 # Q.2) Create a function that takes a list of numbers and returns a new list with each number squared
-
+print("Question-2:\n")
 def squared_number(numbers):
     return [x**2 for x in numbers]
 
@@ -21,7 +21,7 @@ print(f"Squared of a number list {list_num} = {result}")
 print("..............................................\n")
 
 # Q.3) Write a function that checks if a given number is prime
-
+print("Question-3:\n")
 def is_prime(num):
     if num > 0:
         if num % 2 == 0 or num % 3 == 0:
@@ -44,6 +44,7 @@ else:
 print("..............................................\n")
 
 # Q.4) Define a function that takes a list and returns the largest element in the list.
+print("Question-4:\n")
 def largest_element(numbers):
     largest = max(numbers)
     return largest
@@ -55,7 +56,7 @@ print("..............................................\n")
 
 
 # Q.5) Implement a function that calculates the sum of all even numbers in a given range.
-
+print("Question-5:\n")
 def sum_of_even(size):
     sum = 0
     for i in range(1, size+1):
@@ -69,6 +70,7 @@ print(f"Sum of even number upto {num} = {result}")
 print("..............................................\n")
 
 # Q.6) Write a function that takes a dictionary and returns a list of its keys.
+print("Question-6:\n")
 def list_of_keys(dictionary):
     return [x for x in dictionary.keys()]
 
@@ -82,7 +84,7 @@ print(f"List of all keys in dictionary {personal_info} is given below:\n{result}
 print("..............................................\n")
 
 # Q.7) Create a function that takes a string and returns the string with all vowels removed.
-
+print("Question-7:\n")
 def vowels_removal(sentence):
     vowels_letters = ['a', 'e', 'i', 'o', 'u']
     sentence_list = list(sentence)
@@ -102,6 +104,7 @@ print("\n..............................................\n")
 
 
 # Q.8) Write a function that takes a list of strings and returns the longest string in the list.
+print("Question-8:\n")
 def longest_string(sentences_list):
     char_count_list = []
     for i in sentences_list:
@@ -118,7 +121,7 @@ print("................................................................\n")
 
 
 # Q.9) Implement a function that takes a string and counts how many times each character appears in it
-
+print("Question-9:\n")
 def counting_characters(sentences):
     sentence_dict = {}
     for i in sentences:
@@ -131,7 +134,7 @@ print(result)
 print("................................................................\n")
 
 # Q.10) Write a function that accepts a list and returns a new list with duplicate elements removed
-print("Question-10:")
+print("Question-10:\n")
 def duplicate_remove(given_list):
     new_list = []
 
@@ -145,3 +148,77 @@ result = duplicate_remove(user_list)
 print(result)
 print("................................................................\n")
 
+
+# Q.11) Create a function that takes a list of numbers and returns their average
+print("Question-11:\n")
+
+def list_average(number_list):
+    sum = 0
+    for i in number_list:
+        sum += i
+    
+    return sum / len(number_list)
+
+number_list = [1, 2, 3, 4]
+result = list_average(number_list)
+print(f"Average of teh list of numbers {number_list} is {result}")
+print("................................................................\n")
+
+# Q.12) Write a function that accepts a string and a character and counts how many times the character appears in the string.
+print("Question-12: \n")
+
+def count_character(sentence, ch):
+    total_count = 0
+    for i in sentence:
+        if i == ch in sentence:
+            total_count += 1
+    return total_count
+
+words = "Nishan Limbu"
+character = "i"
+
+result = count_character(words, character)
+
+print(f"'{character}' appears {result} times in {words}")
+print("................................................................\n")
+
+
+# Q.13) Define a function that reverses a list without using built-in reverse methods.
+print("Question-13:\n")
+
+def reverse_list(number_list):
+    return number_list[::-1]
+
+number = [1, 454, 2, 4, 7, 12]
+result = reverse_list(number)
+
+print(result)
+print("................................................................\n")
+
+# Q.14) Write a function that takes two lists and returns their intersection.
+print("Question-14: \n")
+
+def common_list(num_list1, num_list2):
+    
+    return list(set(num_list1).intersection(num_list2))
+
+num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num2 = [2, 4, 6, 8, 10]
+
+result = common_list(num1, num2)
+print(f"Common elements in two list list1 = {num1} and list2 = {num2}\n= {result}")
+print("................................................................\n")
+
+
+# Q.15) Implement a function that takes a string and returns the number of words in it.
+print("Question-15: \n")
+
+def words_number(sentences):
+    sent_list = list(sentences.split(' '))
+
+    return len(sent_list)
+
+sentence = "My name is Nishan Limbu"
+result = words_number(sentence)
+print(f"Total words in '{sentence}' is {result}")
+print("................................................................\n")
