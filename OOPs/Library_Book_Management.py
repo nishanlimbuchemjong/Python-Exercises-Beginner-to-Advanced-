100086d29920f2b4639510deecfa30422978f264
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-03 16:20:20 +0545

    delete books method added on Library_Book_Management.py file

--- OOPs/Library_Book_Management.py
+++ OOPs/Library_Book_Management.py
@@ -1,20 +1,15 @@
-# Design a class Library to manage books.
-# Input:
-# Add books: Python Basics, AI Guide
-
-# Search: Python Basics
-# Output:
-# Book found: Python Basics
-
+# created a class 'Library' with attributes book_name, and price
 class Library:
     def __init__(self, book_name, price):
         self.book_name = book_name
         self.price = price
 
+# created another class 'Book' 
 class Book:
     def __init__(self):
         self.books = []
 
+    # method that display all books
     def display_books(self):
         if self.books == []:
             print("No books found!!!")
@@ -22,12 +17,29 @@ class Book:
             for i, book in enumerate(self.books, start=1):
                 print(f"{i} : {book.book_name} - RS. {book.price}")
 
+    # method that add books into a list 'books'
     def add_books(self, book_name, price):
         new_book = Library(book_name, price)
         self.books.append(new_book)
         print("\nBook added successfully!!\n")
 
+    # method to delete books from books list
+    def delete_books(self, book_name):
+        for i in self.books:
+            if book_name in self.books:
+                self.books.remove(book_name)
+        print(f"{book_name} deleted successfully")
+
+# function that validates the integer number
+def get_interger_number(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("\nPlease Enter a positive integer number\n")
+
 def main():
+    # created an instance of a class 'Book'
     book = Book()
 
     while True:
@@ -40,18 +52,20 @@ def main():
         print("3. Delete Books")
         print("4. Exit")
         print("................................................\n")
-        choose = int(input("Choose any options: "))
+
+        choose = get_interger_number("Choose any options: ")
         print("\n")
         if choose == 1:
             users_books = input("Enter Book name: ")
-            user_books_price = int(input("Enter its price: "))
+            user_books_price = get_interger_number("Enter its price: ")
             book.add_books(users_books, user_books_price)
             
         elif choose == 2:
             book.display_books()
             print("\n")
         elif choose == 3:
-            pass
+            users_books = input("Enter Book name: ")
+            book.delete_books(users_books)
         elif choose == 4:
             print("Exiting from Library Management System...........\n")
             break
